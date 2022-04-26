09bdd7fb17b4efc2d8c4134f229122d106683dbf
LFHarada <lucasfigueiraharada@protonmail.com>
2022-04-26 04:49:18 -0300

    commit

--- Project/geradores/cnpj.py
+++ Project/geradores/cnpj.py
@@ -1,5 +1,6 @@
 from random import randint
 
+
 def gerador_cnpj():
 
     print("Matriz ou filial?")
@@ -7,7 +8,7 @@ def gerador_cnpj():
     print("2 - Filial")
 
     try:
-        origem = int(input("Número da escolha: "))
+        escolha = int(input("Número da escolha: "))
 
         dig1 = randint(0, 9)
         dig2 = randint(0, 9)
@@ -19,13 +20,15 @@ def gerador_cnpj():
         dig8 = randint(0, 9)
         dig9, dig10 = 0, 0
 
-        if origem == 1:
+        # escolha matriz ou filial
+        if escolha == 1:
             dig11 = 0
             dig12 = 1
-        if origem == 2:
+        if escolha == 2:
             dig11 = randint(0, 9)
             dig12 = randint(2, 9)
 
+        # Primeiro dígito verificador
         multdig13 = ((0*6)+(dig1*5)+(dig2*4)+
                      (dig3*3)+(dig4*2)+(dig5*9)+
                      (dig6*8)+(dig7*7)+(dig8*6)+
@@ -33,19 +36,26 @@ def gerador_cnpj():
 
         divdig13 = multdig13 % 11
 
-        dig13 = divdig13
+        dig13 = 11 - divdig13
 
+        # Segundo dígito verificador
         multdig14 = ((dig1*6)+(dig2*5)+(dig3*4)+
                      (dig4*3)+(dig5*2)+(dig6*9)+
                      (dig7*8)+(dig8*7)+(dig9*6)+
                      (dig10*5)+(dig11*4)+(dig12*3)+(dig13*2))
 
+        divdig14 = multdig14 % 11
 
-        cpfcompleto = "{}{}.{}{}{}.{}{}{}/{}{}{}{}-{}{}\n".format(
+        dig14 = 11 - divdig14
+
+        cnpjcompleto = "{}{}.{}{}{}.{}{}{}/{}{}{}{}-{}{}\n".format(
             dig1, dig2, dig3, dig4, dig5, dig6, dig7, dig8, dig9, dig10, dig11, dig12, dig13, dig14
         )
-        print(cpfcompleto)
-        return cpfcompleto
+        print(cnpjcompleto)
+        return cnpjcompleto
 
     except:
         print("Por favor, escolha um número válido.\n")
+
+
+gerador_cnpj()
